b9ca01d209680b5b2af5772fbf4551c674cfc0eb
DH-Josiah <92594116+DH-Josiah@users.noreply.github.com>
2021-11-30 22:18:07 -0600

    Add files via upload

---
+++ AoC/2020/Day 1/Day1p1.ps1
@@ -0,0 +1,10 @@
+﻿$input = gc 'C:\Scripts\AoC\2020\Day 1\day1.txt'
+
+for($i = 1; $i -lt $input.Length; $i++){
+    for($i2 = [int]$i+1; $i2 -lt $input.Length; $i2++){
+        if([int]$input[$i]+[int]$input[$i2]+[int]$input[$i3] -eq 2020){
+            Write-Host $input[$i] "*" $input[$i2] "=" $([int]$input[$i]*[int]$input[$i2])
+            exit
+        }
+    }
+}
---
+++ AoC/2020/Day 1/Day1p2.ps1
@@ -0,0 +1,12 @@
+﻿$input = gc 'C:\Scripts\AoC\2020\Day 1\day1.txt'
+
+for($i = 1; $i -lt $input.Length; $i++){
+    for($i2 = [int]$i+1; $i2 -lt $input.Length; $i2++){
+        for($i3 = [int]$i2+1; $i3 -lt $input.Length; $i3++){
+            if([int]$input[$i]+[int]$input[$i2]+[int]$input[$i3] -eq 2020){
+                Write-Host $input[$i] "*" $input[$i2] "*" $input[$i3] "=" $([int]$input[$i]*[int]$input[$i2]*[int]$input[$i3])
+                exit
+            }
+        }
+    }
+}
